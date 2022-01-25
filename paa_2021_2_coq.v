fa5854858f6a764d156186029e940dbb2a3f5b9f
Flávio L. C. de Moura <contato@flaviomoura.mat.br>
2022-01-25 20:40:55 -0300

    definições iniciais

--- paa_2021_2_coq.v
+++ paa_2021_2_coq.v
@@ -1,7 +1,36 @@
-(** * O assistente de provas Coq *)
+(** * Projeto e Análise de Algoritmos  *)
 
-(**
-   Comentários
+(** ** O algoritmo de ordenação por inserção
 *)
 
+Require Import List.
+Require Import Arith.
 
+(**
+   Definição da função de inserção
+*)
+
+Fixpoint insere x l :=
+  match l with
+  | nil => x :: nil 
+  | h :: tl => if (x <=? h) then x :: l else h :: (insere x tl)
+  end.
+
+Eval compute in (insere 3 (1::2::nil)).
+
+Eval compute in (insere 3 (2::1::nil)).
+
+Fixpoint ord_insercao l :=
+  match l with
+  | nil => nil
+  | h :: tl => insere h (ord_insercao tl)
+  end.
+
+Eval compute in (ord_insercao (3::2::1::nil)).
+
+Print list.
+
+
+
+
+Theorem correcao_ord_insercao: forall l, ordenada (ord_insercao l) /\ permutacao l (ord_insercao l).
