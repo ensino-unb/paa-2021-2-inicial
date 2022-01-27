7cbaaaa0c1d5fe9208c031bc61839903a514f0ac
Flávio L. C. de Moura <contato@flaviomoura.mat.br>
2022-01-27 20:42:19 -0300

    prova insere preserva ordem iniciada

--- paa_2021_2_coq.v
+++ paa_2021_2_coq.v
@@ -20,6 +20,39 @@ Eval compute in (insere 3 (1::2::nil)).
 
 Eval compute in (insere 3 (2::1::nil)).
 
+Inductive sorted: list nat -> Prop :=
+| sorted_nil: sorted nil
+| sorted_one: forall x, sorted (x::nil)
+| sorted_all: forall l x y, x <=? y = true -> sorted (y::l) -> sorted (x::y::l).
+
+Lemma insere_preserva_ordem: forall l x, sorted l -> sorted (insere x l).
+Proof.
+  induction l. (* A prova é por indução em l... *)
+  - intro x. 
+    intro H. 
+    simpl.
+    apply sorted_one.
+  - intros x H.
+    simpl.
+    destruct (x <=? a) eqn:Hle.
+    + apply sorted_all.
+      * assumption.
+      * exact H.
+    + generalize dependent l.
+      intro l. case l.
+      * intros IH H.
+        simpl.
+        apply sorted_all.
+        ** clear IH H.
+           apply Nat.leb_gt in Hle.
+           apply Nat.leb_le.
+           apply Nat.lt_le_incl.
+           assumption.
+        ** apply sorted_one.
+      * Admitted.
+
+Print insere_preserva_ordem.
+  
 Fixpoint ord_insercao l :=
   match l with
   | nil => nil
@@ -33,4 +66,4 @@ Print list.
 
 
 
-Theorem correcao_ord_insercao: forall l, ordenada (ord_insercao l) /\ permutacao l (ord_insercao l).
+Theorem correcao_ord_insercao: forall l, sorted (ord_insercao l) /\ permutacao l (ord_insercao l).
