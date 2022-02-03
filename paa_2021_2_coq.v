af3e75eb37265adfd1fc9ee4a79cf3dfaee4acb8
Flávio L. C. de Moura <contato@flaviomoura.mat.br>
2022-02-03 20:38:00 -0300

    perm'

--- paa_2021_2_coq.v
+++ paa_2021_2_coq.v
@@ -88,4 +88,131 @@ Proof.
     apply IHl.
 Qed.
 
-Theorem correcao_ord_insercao: forall l, sorted (ord_insercao l) /\ permutacao l (ord_insercao l).
+(*
+Inductive Permutation': list nat -> list nat -> Prop :=
+    perm'_nil : Permutation' nil nil
+  | perm'_skip : forall (x : nat) (l l' : list nat),
+                Permutation' l l' -> Permutation' (x :: l) (x :: l')
+  | perm'_swap : forall (x y : nat) (l : list nat),
+                Permutation' (y :: x :: l) (x :: y :: l)
+  | perm'_trans : forall l l' l'' : list nat,
+                 Permutation' l l' ->
+                 Permutation' l' l'' -> Permutation' l l''.
+
+
+Lemma Permutation'_refl: forall l, Permutation' l l.
+Proof.
+  induction l.
+  - apply perm'_nil.
+  - apply perm'_skip.
+    apply IHl.
+Qed.
+  
+Lemma Permutation'_insere: forall l a, Permutation' (a :: l) (insere a l).
+Proof.
+  induction l.
+  - intro a.
+    simpl.
+    apply Permutation'_refl.
+  - intros a'.
+    simpl.
+    destruct (a' <=? a).
+    + apply Permutation'_refl.
+    + apply perm'_trans with (a::a'::l).
+      * apply perm'_swap.
+      * apply perm'_skip.
+        apply IHl.
+Qed.
+  
+Lemma Permutation'_insere_diff: forall l l' a, Permutation' l l' -> Permutation' (a :: l) (insere a l').
+Proof.
+  intros l l' a H. 
+  apply perm'_skip with (x := a) in H.
+  apply perm'_trans with (a::l').
+  - assumption.
+  - apply Permutation'_insere.
+Qed.
+*)
+
+Require Import Permutation.
+Print Permutation.
+
+
+Lemma Permutation_insere: forall l a, Permutation (a :: l) (insere a l).
+Proof.  
+  induction l.
+  - intro a.
+    simpl.
+    apply Permutation_refl.
+  - intros a'.
+    simpl.
+    destruct (a' <=? a).
+    + apply Permutation_refl.
+    + apply perm_trans with (a::a'::l).
+      * apply perm_swap.
+      * apply perm_skip.
+        apply IHl.
+Qed.
+  
+Lemma Permutation_insere_diff: forall l l' a, Permutation l l' -> Permutation (a :: l) (insere a l').
+Proof.
+  intros l l' a H.
+  rewrite H.
+  apply Permutation_insere.
+Qed.
+
+Lemma ord_insercao_Permutation: forall l, Permutation l (ord_insercao l).
+Proof.
+  induction l.
+  - simpl.
+    apply perm_nil.
+  - simpl.
+    apply Permutation_insere_diff.
+    apply IHl.
+Qed.
+  
+Theorem correcao_ord_insercao: forall l, sorted (ord_insercao l) /\ Permutation l (ord_insercao l).
+Proof.
+  intro l; split.
+  - apply ord_insercao_preserva_ordem.
+  - apply ord_insercao_Permutation.
+Qed.
+
+Fixpoint num_oc x l := match l with
+                       | nil => 0
+                       | h::tl => if (x =? h) then S(num_oc x tl) else num_oc x tl
+                       end.
+
+Eval compute in (num_oc 2 (1::2::3::2::2::nil)).
+
+Definition perm' l l' := forall x, num_oc x l = num_oc x l'.
+
+Lemma num_oc_insere: forall l l' x a, perm' l l' -> (if x =? a then S (num_oc x l) else num_oc x l) =
+  num_oc x (insere a l').
+Proof.
+Admitted.
+
+(* Abordagem alternativa:
+
+Lemma num_oc_insere_eq: num_oc a (insere a l) = S(num_oc a l)
+
+Lemma num_oc_insere_diff: x <> a ->  num_oc x (insere a l)
+  *)  
+
+Lemma ord_insercao_perm': forall l, perm' l (ord_insercao l).
+Proof.
+  induction l.
+  - simpl.
+    unfold perm'.
+    intro x.
+    reflexivity.
+  - simpl.
+    unfold perm' in *.
+    intro x.
+    simpl.
+    apply num_oc_insere.
+    unfold perm'.
+    intro x'.
+    apply IHl.
+Qed.
+
